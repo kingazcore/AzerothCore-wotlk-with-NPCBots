0903369b04d3cc3cdb954b8d68e6348158b848a7
jlgao2 <jinlgao@126.com>
2023-11-12 14:16:20 +0800

    修改世界服务配置

--- custom/mod/clone.sh
+++ custom/mod/clone.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/kingazcore/mod-transmog.git
 git clone https://github.com/kingazcore/mod-anticheat.git
 git clone https://github.com/kingazcore/mod-eluna.git
-git clone https://github.com/kingazcore/mmod-morphsummon.git
+git clone https://github.com/kingazcore/mod-morphsummon.git
