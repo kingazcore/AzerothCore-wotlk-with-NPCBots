0dbb8ddeb0c6bdc368e25b09af16d8bbe664bd05
jlgao2 <jinlgao@126.com>
2023-11-12 11:40:42 +0800

    移除git子模块

---
+++ custom/mod/clone.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/kingazcore/mod-transmog.git
+git clone https://github.com/kingazcore/mod-anticheat.git
+git clone https://github.com/kingazcore/mod-eluna.git
+git clone https://github.com/kingazcore/mmod-morphsummon.git
